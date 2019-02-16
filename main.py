281ec27aff3ab265f76282e9fa219a6d35ef7c91
arit007 <46465730+arit007@users.noreply.github.com>
2019-02-15 19:33:38 -0600

    finished build-a-blog

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
-import cgi
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Blogging01@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = 'nachocheese'
+app.secret_key = 'nAcHoChEeS3'
 
 class Blog(db.Model):    
     id = db.Column(db.Integer, primary_key = True)
@@ -17,38 +17,45 @@ class Blog(db.Model):
     def __init__(self, title, body):
         self.title = title
         self.body = body
-
+       
+#display all blog posts
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
-    posts = Blog.query.all()
-    post_id = request.args.get("id")
-    if post_id:
-        post = Blog.query.get(post_id)
-        return render_template("selectedpost.html", post=post)
-        return render_template("blog.html", posts=posts)
+    blog_id = request.args.get('id')
+    blogs = Blog.query.all()
+    if blog_id is None:
+        return render_template('blog.html', blogs=blogs)
+    else: 
+        
+        blog = Blog.query.get(blog_id) #changed from blogs to blog
+        return render_template('selectedpost.html', blog=blog)
 
+#after submitting new post display on main blog page    
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
-    if request.method == "POST":
+    if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        if title == "":
-            flash("Please title your blog post")
-            return redirect ('/newpost')
-        if body == "":
-            flash("Please don't leave your post empty")
+        if title and body == "":
+            flash("Please don't leave post empty")
             return redirect('/newpost')
-        if len(title) > 0 and len(body) > 0:
-            new_post = Blog(title, body)
-            db.session.add(new_post)
+        else:
+            new_blog = Blog(title, body)
+            db.session.add(new_blog)
             db.session.commit()
-            posts.url = "/blog?id" + str(new_post.id)   
-            return redirect("selectedpost.html")
-    return render_template("newpost.html")        
+        blogs = Blog.query.all()
+        return render_template('blog.html', blogs=blogs)   
+    return render_template("newpost.html")    
  
-@app.route('/')
+@app.route('/', methods =['POST', 'GET'])
 def index():
-    return redirect('/blog')
+    if request.method == 'POST':
+        blog_name = request.form['blog']
+        new_blog = Blog(blog_name)
+        db.session.add(new_blog)
+        db.session.commit()
+    blogs = Blog.query.all()
+    return render_template('blog.html', blogs=blogs)    
 
 if __name__ == '__main__':
     app.run()
